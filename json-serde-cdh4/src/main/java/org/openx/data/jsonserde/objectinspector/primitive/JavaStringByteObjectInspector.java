6ee04f3db3fb2fdc884ff419adaa16755fd5adb6
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-03-02 15:29:21 -0600

    Added getPrimitiveJavaObject method to all data types to avoid the class casting exception

--- json-serde-cdh4/src/main/java/org/openx/data/jsonserde/objectinspector/primitive/JavaStringByteObjectInspector.java
+++ json-serde-cdh4/src/main/java/org/openx/data/jsonserde/objectinspector/primitive/JavaStringByteObjectInspector.java
@@ -49,6 +49,11 @@ public  class JavaStringByteObjectInspector
     }
 
     @Override
+    public Object getPrimitiveJavaObject(Object o) {
+        return get(o);
+    }
+
+    @Override
     public Object create(byte value) {
         return (value);
     }
